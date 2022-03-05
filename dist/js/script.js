cdbff1969f1e1630f7d6aadf2d4d24fad737da25
Ramil Galikeev <galikeevramil95@gmail.com>
2022-03-05 20:58:40 +0500

    added more styles

--- dist/js/script.js
+++ dist/js/script.js
@@ -125,41 +125,52 @@ const addDelRow = () => {
   const table = document.querySelector('.table');
   const delBtn = document.querySelector('.button__del');
   let count = 1;
-  addBtn.addEventListener('click', () => {
-    count++;
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <td>
-            <input name="name${count}" type="text" required class="table__name">
-        </td>
-        <td>
-            <select name="post${count}" id="#">
-                <option value="1">менеджер</option>
-                <option value="1">аналитик</option>
-                <option value="1">программист</option>
-                <option value="1">юрист</option>
-            </select>
-        </td>
-        <td>
-            <input name="age${count}" type="text" required class="table__age">
-        </td>
-        <td>
-            <input name="text${count}" type="text">
-        </td>
-        `;
-    table.appendChild(row);
 
-    if (table.childNodes.length >= 3) {
-      delBtn.style.display = 'block';
-    }
-  });
-  delBtn.addEventListener('click', () => {
-    table.lastChild.remove();
+  const addLineTable = () => {
+    addBtn.addEventListener('click', () => {
+      count++;
+      const row = document.createElement('tr');
+      row.classList.add('table__body');
+      row.innerHTML = `
+            <td>
+                <input name="name${count}" type="text" required class="table__name input">
+            </td>
+            <td>
+                <select name="post${count}" id="#" class="input">
+                    <option value="1">менеджер</option>
+                    <option value="1">аналитик</option>
+                    <option value="1">программист</option>
+                    <option value="1">юрист</option>
+                </select>
+            </td>
+            <td>
+                <input name="age${count}" type="text" required class="table__age input">
+            </td>
+            <td>
+                <input name="text${count}" type="text" class="input">
+            </td>
+            `;
+      table.appendChild(row);
 
-    if (table.childNodes.length <= 2) {
-      delBtn.style.display = 'none';
-    }
-  });
+      if (table.childNodes.length >= 3) {
+        delBtn.style.display = 'block';
+      }
+    });
+  };
+
+  addLineTable();
+
+  const deleteLineTable = () => {
+    delBtn.addEventListener('click', () => {
+      table.lastChild.remove();
+
+      if (table.childNodes.length <= 2) {
+        delBtn.style.display = 'none';
+      }
+    });
+  };
+
+  deleteLineTable();
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (addDelRow);
